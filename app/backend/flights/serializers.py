31359af60bb6ef603a324162169dea26d64b51b5
Igor Julliano <igor.julliano2@gmail.com>
2024-03-21 13:33:33 -0300

    feat(backend): group api response into two main lists

--- app/backend/flights/serializers.py
+++ app/backend/flights/serializers.py
@@ -1,8 +1,13 @@
-
 from rest_framework import serializers
 from .models import Flight
+
 
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
-        fields = '__all__'
+        fields = "__all__"
+
+
+class GroupedFlightsSerializer(serializers.Serializer):
+    main = serializers.ListSerializer(child=FlightSerializer())
+    others = serializers.ListSerializer(child=FlightSerializer())
--- app/backend/flights/views.py
+++ app/backend/flights/views.py
@@ -1,20 +1,39 @@
-from rest_framework import viewsets
 from django.db.models.query import QuerySet
+from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import Flight
-from .serializers import FlightSerializer
+from .serializers import FlightSerializer, GroupedFlightsSerializer
 
 
 # Create your views here.
-class FlightsViewSet(viewsets.ModelViewSet):
+class FlightsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+
+    MAIN_FLIGHT_MAX_COUNT = 2
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        data = response.data
+
+        main_flights = data[: self.MAIN_FLIGHT_MAX_COUNT]
+        other_flights = data[self.MAIN_FLIGHT_MAX_COUNT :]
+
+        serializer = GroupedFlightsSerializer(
+            data={
+                "main": main_flights,
+                "others": other_flights,
+            }
+        )
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data)
 
     def get_queryset(self):
         query_set: QuerySet = super().get_queryset()
 
         destination = self.request.GET.get("city")
-        date = self.request.GET.get("date") # TODO: Use date to validate
+        date = self.request.GET.get("date")  # TODO: Use date to validate
 
         if destination is not None and date is not None:
             query_set = query_set.filter(city_destination=destination)
