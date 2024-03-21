ab51ba1ce96d449bdab52ed83f5a57aeddb5dd61
Igor Julliano <igor.julliano2@gmail.com>
2024-03-21 12:31:53 -0300

    feat(backend): filter flights by destination and "date"

--- app/backend/flights/views.py
+++ app/backend/flights/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django.db.models.query import QuerySet
 
 from .models import Flight
 from .serializers import FlightSerializer
@@ -8,3 +9,14 @@ from .serializers import FlightSerializer
 class FlightsViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+
+    def get_queryset(self):
+        query_set: QuerySet = super().get_queryset()
+
+        destination = self.request.GET.get("city")
+        date = self.request.GET.get("date") # TODO: Use date to validate
+
+        if destination is not None and date is not None:
+            query_set = query_set.filter(city_destination=destination)
+
+        return query_set
