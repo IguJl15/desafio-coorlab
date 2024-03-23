be33c2b683169ef2183349f91a04db76e0287f62
Igor Julliano <igor.julliano2@gmail.com>
2024-03-23 09:07:55 -0300

    feat(backend): add jwt tokens authentication/authorization

--- app/backend/travel/urls.py
+++ app/backend/travel/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from flights import urls as flights_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include(flights_urls)),
 ]
