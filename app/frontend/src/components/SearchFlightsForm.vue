055020c62c4fd3bd5c23442463238a3626514115
Igor Julliano <igor.julliano2@gmail.com>
2024-03-24 03:59:21 -0300

    feat(frontend): finish form state management

--- app/frontend/src/components/SearchFlightsForm.vue
+++ app/frontend/src/components/SearchFlightsForm.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { formatVerboseDestination } from '@/misc/helper'
 import type { Airport } from '@/models/Flight'
-import { computed, ref } from 'vue'
+import { reactive, ref, toRaw } from 'vue'
+import { Dayjs } from 'dayjs'
 
-import { Button, Form, FormItem, Input, Select, SelectOption } from 'ant-design-vue'
+import { Button, Form, FormItem, DatePicker, Select, SelectOption } from 'ant-design-vue'
+import type { Rule } from 'ant-design-vue/es/form'
+import type { ValidateErrorEntity } from 'ant-design-vue/es/form/interface'
 import {
   ArrowRightOutlined,
   LoginOutlined,
@@ -13,47 +16,69 @@ import {
 
 import airports from '@/data/airports.json'
 
+const emit = defineEmits<{
+  (e: 'form-button-click', data: FormDataScheme): void
+}>()
+
 export type FormDataScheme = {
   destination: Airport
   date: Date
 }
 
-defineEmits<{
-  (e: 'form-button-click', data: FormDataScheme): void
-}>()
-
-const airportList: Airport[] = [{ code: 'THE', name: 'Teresina' }, ...airports]
-
-const formState = ref<FormDataScheme>({
-  destination: airportList[0],
-  date: new Date()
-})
-
-// Computed references to enhance form data handle
-const dateInputComp = computed({
-  get() {
-    return formState.value.date?.toISOString().substring(0, 10)
-  },
-  // expect string but receives Date from "valueAsDate" property
-  set(newValue) {
-    if (newValue) formState.value.date = newValue as Date
+const formRef = ref()
+const rules: Record<string, Rule> = {
+  origin: { required: true, message: 'Escolha de onde você irá viajar', trigger: 'change' },
+  destination: { required: true, message: 'Escolha o destino da sua viagem', trigger: 'change' },
+  date: {
+    required: true,
+    message: 'Escolha a data da sua viagem',
+    trigger: 'change',
+    type: 'object'
   }
+}
+
+const mockAirport = { code: 'THE', name: 'Teresina' }
+const airportList: Airport[] = [mockAirport, ...airports]
+
+const formState = reactive<{
+  origin: string | undefined
+  destination: string
+  date: Dayjs | undefined
+}>({
+  origin: undefined,
+  destination: airportList[0].name,
+  date: undefined
 })
 
-const destinationInputComp = computed({
-  get() {
-    return formatVerboseDestination(formState.value.destination)
-  },
-  set(newValue: string) {
-    const code = newValue.split(' - ')[0]
+function onSubmit() {
+  console.log('Validating')
 
-    const airportHasCode = (airport: Airport) => airport.code == code
+  formRef.value
+    .validate()
+    .then(() => {
+      console.log('values', formState, toRaw(formState))
+      emit('form-button-click', {
+        date: formState.date!.toDate(),
+        destination: airportList.filter((air) => air.name == formState.destination)[0]
+      })
+    })
+    .catch((error: any) => {
+      console.log('error', error)
+    })
+}
 
-    if (code.length > 1 && airportList.some(airportHasCode)) {
-      formState.value.destination = airportList.filter(airportHasCode)[0]
-    }
-  }
-})
+// dialog
+const dialogRef = ref<HTMLDialogElement>()
+const dialogErrors = ref<string[] | undefined>()
+
+function onFormError(errors: ValidateErrorEntity) {
+  console.log('errors', errors)
+  dialogErrors.value = errors.errorFields.reduce<string[]>((list, err) => {
+    list.push(...err.errors)
+    return list
+  }, [])
+  dialogRef.value?.showModal()
+}
 </script>
 
 <template>
@@ -64,51 +89,55 @@ const destinationInputComp = computed({
     </div>
 
     <Form
-      class="search-form"
+      ref="formRef"
       layout="vertical"
-      @finish="$emit('form-button-click', formState)"
+      class="search-form"
       :model="formState"
+      :rules="rules"
+      @finish="onSubmit"
+      @finish-failed="onFormError"
     >
-      <FormItem
-        class="form-item"
-        label="Destino"
-        :rules="[{ required: true, message: 'Escolha seu destino' }]"
-      >
-        <!-- event has the value of the "value" prop from SelectOption  -->
-        <Select
-          show-search
-          name="destination"
-          :value="destinationInputComp"
-          @change="
-            (event) => {
-              destinationInputComp = event?.toString() ?? ''
-            }
-          "
-        >
-          <template #placeholder> <LogoutOutlined style="color: black" /> Busca </template>
-          <template
-            v-for="(item, index) in airportList.sort((a, b) => a.name.localeCompare(b.name))"
-            :key="index"
-          >
-            <SelectOption :value="formatVerboseDestination(item)">
-              {{ item.name }}
+      <FormItem class="form-item" ref="origin" name="origin" label="Origem">
+        <div class="input-row-icon">
+          <Select show-search placeholder="De onde viemos" v-model:value="formState.origin">
+            <SelectOption :value="mockAirport.name">
+              {{ mockAirport.name }}
             </SelectOption>
-          </template>
-        </Select>
+          </Select>
+          <LogoutOutlined />
+        </div>
       </FormItem>
-      <ArrowRightOutlined style="color: white" />
-      <FormItem class="form-item" label="Data da viagem">
-        <Input
-          name="date"
+      <ArrowRightOutlined :class="'arrow'" style="color: white" />
+      <FormItem class="form-item" ref="destination" name="destination" label="Destino">
+        <!-- event has the value of the "value" prop from SelectOption  -->
+        <div class="input-row-icon">
+          <LoginOutlined />
+          <Select
+            show-search
+            placeholder="Encontre seu destino"
+            v-model:value="formState.destination"
+          >
+            <template
+              v-for="(item, index) in airportList.sort((a, b) => a.name.localeCompare(b.name))"
+              :key="index"
+            >
+              <SelectOption :value="formatVerboseDestination(item)">
+                {{ item.name }}
+              </SelectOption>
+            </template>
+          </Select>
+        </div>
+      </FormItem>
+      <FormItem class="form-item" ref="date" name="date" label="Data da viagem">
+        <DatePicker
           type="date"
           placeholder="Procure a cidade de destino"
-          @input="(event) => (dateInputComp = (event.target! as HTMLInputElement).valueAsDate)"
-          :value="dateInputComp"
+          v-model:value="formState.date"
         >
-          <template #prefix>
+          <!-- <template #prefix>
             <LoginOutlined />
-          </template>
-        </Input>
+          </template> -->
+        </DatePicker>
       </FormItem>
       <Button class="button" type="primary" html-type="submit">
         <template #icon>
@@ -118,6 +147,15 @@ const destinationInputComp = computed({
       </Button>
     </Form>
   </div>
+  <dialog class="dialog" ref="dialogRef">
+    <strong>Insira os dados corretos para pesquisar por voos disponíveis</strong>
+    <ul>
+      <li v-for="(item, index) in dialogErrors" :key="index">{{ item }}</li>
+    </ul>
+    <form method="dialog">
+      <Button type="default" autofocus html-type="submit"> OK</Button>
+    </form>
+  </dialog>
 </template>
 
 <style scoped>
@@ -129,7 +167,8 @@ const destinationInputComp = computed({
   padding: 16px;
   border-radius: 12px;
 
-  background-color: var(--color-background-soft);
+  background-color: var(--primary-color);
+  color: var(--vt-c-text-dark-1);
 
   & h3,
   p {
@@ -142,20 +181,50 @@ const destinationInputComp = computed({
     place-items: center;
     gap: 8px;
 
-    & .button {
+    & .button,
+    .arrow {
       align-self: end;
 
       margin-bottom: 6px;
+    }
+    & .arrow {
+      margin-bottom: 16px;
     }
 
     & .form-item {
       width: 100%;
 
       background-color: var(--color-background);
-      padding: 6px;
+      padding: 6px 8px;
       border-radius: 6px;
 
       margin: 0;
+    }
+  }
+}
+
+.input-row-icon {
+  display: flex;
+  gap: 8px;
+}
+
+.dialog {
+  background-color: var(--color-background);
+  color: var(--color-text);
+  border: none;
+  border-radius: 12px;
+  padding: 12px;
+}
+
+@media (prefers-color-scheme: dark) {
+  .search-form-container {
+    background-color: var(--primary-color-dark);
+    color: var(--vt-c-text-dark-1);
+
+    & .search-form {
+      & .form-item {
+        background-color: var(--color-background);
+      }
     }
   }
 }
